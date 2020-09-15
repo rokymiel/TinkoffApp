e4107521536a1cce3314ba225543cd27e590bffa
rokymiel <rokymiel@gmail.com>
2020-09-15 22:12:08 +0300

    Добавлены комментарии в класс Логов

--- TinkoffApp/Logger.swift
+++ TinkoffApp/Logger.swift
@@ -9,11 +9,23 @@
 import Foundation
 
 class Logger {
+    /**
+        - description:Вывод сообщения с информацией о переходе State для application
+        - parameters:
+                - from: переход из данного состояния
+                - to: переход в данное состояние
+    */
     static func logFunctionName(from:String,to:String,functionName: String = #function) {
         #if DEBUG
         print("Application moved from \(from) to \(to): \(functionName)")
         #endif
     }
+    /**
+        - description: Логирование с выводом информационного сообщения
+        - parameters:
+            - with : сообщение для вывода
+            - functionName: имя функции
+    */
     static func logFunctionName(with message:String,functionName: String = #function) {
         #if DEBUG
         print(message+":"+functionName)
